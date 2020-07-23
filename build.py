628f9261faaeb29b0a33290c3a37a2d0120f16fc
My <34188127+0xcaffebabe@users.noreply.github.com>
2020-07-23 16:46:31 +0800

    Update build.py

--- build.py
+++ build.py
@@ -2,7 +2,7 @@ import httpx
 
 def fetch_code_time():
     return httpx.get(
-        "https://gist.githubusercontent.com/0xcaffebabe/8ea5a71947543404d826b1a839b29398/raw/351d60fae06291fc5200c7cb5128439f9f5d794b/%25F0%259F%2593%258A%2520Weekly%2520development%2520breakdown"
+        "https://gist.githubusercontent.com/0xcaffebabe/8ea5a71947543404d826b1a839b29398/raw"
     )
 
 readme = "# Overview\n\n"+\
