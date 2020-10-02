ddabae7ad8f9b4007a0d1d473ad42a6a0566b909
MY <715711877@qq.com>
2020-10-02 17:06:58 +0800

    update

--- README.md
+++ README.md
@@ -1,6 +1,35 @@
-# Overview
 
-📊 Weekly development breakdown
+## Overview!
+
+#### 📖 最近博客
+
+<!-- blog starts -->
+
+* <a href="https://ismy.wang/%E7%AE%97%E6%B3%95/2020/09/18/%E8%B0%88%E8%B0%88bitmap%E7%9A%84%E4%BD%BF%E7%94%A8.html" target="_blank"> 谈谈bitmap的使用 </a> - 2020/09/18 
+
+    
+* <a href="https://ismy.wang/%E8%AE%BE%E8%AE%A1%E6%A8%A1%E5%BC%8F/2020/09/11/%E6%A1%A5%E6%8E%A5%E6%A8%A1%E5%BC%8F%E7%9A%84%E5%AE%9E%E9%99%85%E4%BD%BF%E7%94%A8.html" target="_blank"> 桥接模式的实际使用 </a> - 2020/09/11 
+
+    
+* <a href="https://ismy.wang/java/2020/04/16/JAVA%E5%AE%9E%E7%8E%B0%E8%8A%82%E6%B5%81%E9%98%80.html" target="_blank"> java实现节流阀 </a> - 2020/04/16 
+
+    
+* <a href="https://ismy.wang/%E6%97%A5%E5%B8%B8/2020/04/16/%E8%B0%83%E8%AF%95%E4%B8%80%E4%B8%AAwsl2+java%E5%BC%95%E5%8F%91%E7%9A%84bug.html" target="_blank"> 调试一个wsl2+java引发的bug </a> - 2020/04/16 
+
+    
+* <a href="https://ismy.wang/spring/2019/11/04/Spring-%E5%9B%9B-%E4%BA%8B%E5%8A%A1%E7%AE%A1%E7%90%86.html" target="_blank"> Spring(四):事务管理 </a> - 2019/11/04 
+
+    
+<!-- blog ends -->
+
+</td>
+</tr>
+<tr>
+<td valign="top" width="50%">
+
+#### 📊 最近一周开发时间
+
+<!-- code_time starts -->
 
 ```text
 Markdown    19 hrs 41 mins ███████████▋░░░░░░░░░  55.7%
@@ -9,3 +38,10 @@ Go          3 hrs 33 mins  ██░░░░░░░░░░░░░░░�
 JavaScript  1 hr 26 mins   ▊░░░░░░░░░░░░░░░░░░░░   4.1%
 HTML        58 mins        ▌░░░░░░░░░░░░░░░░░░░░   2.8%
 ```
+
+<!-- code_time ends -->
+
+</td>
+
+  </tr>
+  </table>
--- build.py
+++ build.py
@@ -1,16 +1,65 @@
 import httpx
+from bs4 import BeautifulSoup
 
 def fetch_code_time():
     return httpx.get(
         "https://gist.githubusercontent.com/0xcaffebabe/8ea5a71947543404d826b1a839b29398/raw"
     )
 
-readme = "# Overview\n\n"+\
-"📊 Weekly development breakdown\n\n"+\
-"```text\n"+\
-  fetch_code_time().text+"\n"+\
-"```"
+def fetch_recent_blog():
+  text = httpx.get('https://ismy.wang').text
+  soup = BeautifulSoup(text)
+  postList = soup.select(".post-list li")
+  count = 0
+  html = ""
+  for i in postList:
+    if count >= 5:
+      break
+    blogTitle = i.a.text
+    blogLink = i.a['href']
+    blogDate = i.select('.post-list-meta')[0].text
+    str = """
+* <a href="${link}" target="_blank"> ${title} </a> - ${date} \n
+    """
+    str = str.replace('${link}', blogLink.strip())
+    str = str.replace('${title}', blogTitle.strip())
+    str = str.replace('${date}', blogDate.strip())
+    html += str
+    count = count + 1
+  return html
 
+readmeTemplate = """
+## Overview!
+
+#### 📖 最近博客
+
+<!-- blog starts -->
+${recent_blogs}
+<!-- blog ends -->
+
+</td>
+</tr>
+<tr>
+<td valign="top" width="50%">
+
+#### 📊 最近一周开发时间
+
+<!-- code_time starts -->
+
+```text
+${code_time}
+```
+
+<!-- code_time ends -->
+
+</td>
+
+  </tr>
+  </table>
+"""
+
+readme = readmeTemplate.replace('${code_time}', fetch_code_time().text)
+readme = readme.replace("${recent_blogs}",fetch_recent_blog())
 print (readme)
 
-open('./README.md','w').write(readme)
+open('./README.md','w',encoding="utf8").write(readme)
